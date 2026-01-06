42c2cf828d3fc37b6f90f3021319ddb5ec8e0eee
mediaclockdev <sb@mediaclock.com.au>
2026-01-06 19:24:32 +1100

    changes

--- app/Helpers/CommonHelper.php
+++ app/Helpers/CommonHelper.php
@@ -87,7 +87,7 @@ if (!function_exists('siteLogo')) {
     }
     $logo = SiteSetting::where('key', 'site_logo')->value('value');
     // return $logo ? url('public/storage/uploads/site/logo/' . $logo) : asset('public/backend/assetss/images/logo-dark.png');
-    return $logo ? url('public/frontend/assets/img/mayeri-logo.png') : asset('public/frontend/assets/img/mayeri-logo.png');
+    return $logo ? url('public/frontend/assets/img/New_World.webp') : asset('public/frontend/assets/img/New_World.webp');
   }
 }
 
