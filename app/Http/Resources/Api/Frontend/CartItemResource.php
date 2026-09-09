19f5c92e3d9d71be741ba8b7ecb9ab7850d30b9f
mediaclockdev <sb@mediaclock.com.au>
2026-09-09 14:47:09 +1000

    changes

--- app/Http/Resources/Api/Frontend/CartItemResource.php
+++ app/Http/Resources/Api/Frontend/CartItemResource.php
@@ -25,7 +25,7 @@ class CartItemResource extends JsonResource
 
     $totalPrice = $unitPrice * $qty;
 
-    $attributes = $variant->variantAttributes ?? [];
+    $attributes = $variant->attribute_details ?? [];
 
     return [
       'id' => Hashids::encode($this->id),
